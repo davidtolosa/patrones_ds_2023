fe2831cfc76eee6122eeb0d2b98aaa9ac5092b1f
David Tolosa <davidtolosa5@gmail.com>
2023-09-27 22:47:58 -0300

    Builder

---
+++ src/creacionales/builder/Documentacion.java
@@ -0,0 +1,11 @@
+package creacionales.builder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Documentacion {
+    protected List<String> contenido = new ArrayList<String>();
+    
+    public abstract void agregaDocumento(String documento);
+    public abstract void imprime();
+}
---
+++ src/creacionales/builder/Vendedor.java
@@ -0,0 +1,16 @@
+package creacionales.builder;
+
+public class Vendedor {
+    protected ConstructorDocumentacionVehiculo constructor;
+
+    public Vendedor(ConstructorDocumentacionVehiculo constructor) {
+        this.constructor = constructor;
+    }
+
+    public Documentacion construye(String nombreCliente) {
+        constructor.construyeSolicitudPedido(nombreCliente);
+        constructor.construyeSolicitudMatriculacion(nombreCliente);
+        Documentacion documentacion = constructor.resultado();
+        return documentacion;
+    }
+}
