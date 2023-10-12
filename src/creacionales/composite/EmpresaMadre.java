3e98aff9860250a57791cf861c751e0ad494629a
David Tolosa <davidtolosa5@gmail.com>
2023-10-11 23:14:25 -0300

    Composite

---
+++ src/creacionales/composite/EmpresaMadre.java
@@ -0,0 +1,23 @@
+package creacionales.composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmpresaMadre extends Empresa {
+
+    protected List<Empresa> filiales = new ArrayList<Empresa>();
+
+    @Override
+    public double calculaCosteMantenimiento() {
+        double coste = 0.0;
+        for (Empresa filial : filiales)
+            coste = coste + filial.calculaCosteMantenimiento();
+        return coste + nVehiculos * costeUnitarioVehiculo;
+    }
+
+    @Override
+    public boolean agregaFilial(Empresa filial) {
+        return filiales.add(filial);
+    }
+
+}
---
+++ src/creacionales/composite/EmpresaSinFilial.java
@@ -0,0 +1,15 @@
+package creacionales.composite;
+
+public class EmpresaSinFilial extends Empresa {
+
+    @Override
+    public double calculaCosteMantenimiento() {
+        return nVehiculos * costeUnitarioVehiculo;        
+    }
+
+    @Override
+    public boolean agregaFilial(Empresa filial) {
+        return false;
+    }
+    
+}
